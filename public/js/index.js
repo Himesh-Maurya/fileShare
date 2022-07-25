d91401a6ecc2857985ca3d5caa4fa143c2e4ff01
Himesh-Maurya <himeshmauurya@gmail.com>
2022-07-25 17:36:50 +0530

    add link

--- public/js/index.js
+++ public/js/index.js
@@ -15,8 +15,8 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-// const baseURL = "https://innshare.herokuapp.com";
-const baseURL = "http://localhost:3000";
+const baseURL = "https://fileshare7.herokuapp.com/";
+// const baseURL = "http://localhost:3000";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
