bd1977fb7a99613093bb81c2ee65cd0018beb701
Himesh-Maurya <himeshmauurya@gmail.com>
2022-07-25 18:47:58 +0530

    add

--- routes/files.js
+++ routes/files.js
@@ -78,9 +78,11 @@ router.post('/send', async (req, res) => {
     }).then(() => {
       return res.json({success: true});
     }).catch(err => {
+      console.log(err)
       return res.status(500).json({error: 'Error in email sending.'});
     });
 } catch(err) {
+  console.log(err)
   return res.status(500).send({ error: 'Something went wrong.'});
 }
 
